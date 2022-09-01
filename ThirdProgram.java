7d743fdd41cf2045aceca58908daeb32ae060f06
Enoch Adelekan <86803876+dieko-ade@users.noreply.github.com>
2022-09-01 10:18:41 -0400

    Add files via upload

---
+++ ThirdProgram.java
@@ -0,0 +1,9 @@
+class ThirdProgram {
+    public static void main (String[] args) {
+        int[] a = new int[10];
+        for (int i = 0; i < a.length; i++) {
+            System.out.println ("a[" + i + "] = " + a[i]);
+        }
+        // a = null;
+    }
+}
